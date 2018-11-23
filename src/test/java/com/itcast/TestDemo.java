87fe07ca66e9bf817032fca784f42132c2f119fb
houlin <771691615@qq.com>
2018-11-23 10:20:42 +0800

    嘿嘿嘿more

--- src/test/java/com/itcast/TestDemo.java
+++ src/test/java/com/itcast/TestDemo.java
@@ -5,6 +5,9 @@ public class TestDemo {
     public static void main(String[] args) {
 
         System.out.println("嘿嘿嘿");
+        System.out.println("嘿嘿嘿");
+        System.out.println("嘿嘿嘿");
+        System.out.println("嘿嘿嘿");
 
     }
 
